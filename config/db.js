e6a57e1d7eeac83a9c4d9bd1b45aa6cb59cd509b
NicoMachado <nico.machado@gmail.com>
2021-06-08 13:50:42 -0300

    Ajuste db.js

--- config/db.js
+++ config/db.js
@@ -21,6 +21,12 @@ const db = new Sequelize(
         port: process.env.BD_PORT,
         define: {
             timestamps: false
+        },
+        pool:{
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle:10000
         }
     }
 );
